94cc1da8a8c18278e0c00b323363ae75a91b0e71
NikPaukov <sirelfarionik@gmail.com>
2022-06-10 20:47:14 +0300

    a

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def help(update, context):
     if user_id not in userDB.keys():
         userDB.update({user_id: username})
     if 'vm.tiktok.com' in update.message.text:
-        rand_user = str(random.choice(list(userDB.keys())))
+        rand_user = random.choice(list(userDB.keys()))
 
         update.message.reply_text(
-            "[Згоден, " + userDB[rand_user] + "?" + rand_user + "](tg://user?id=" + rand_user + ")",
+            "[Згоден, " + userDB[rand_user] + "?" + str(rand_user) + "](tg://user?id=" + rand_user + ")",
             parse_mode="Markdown")
 
 
